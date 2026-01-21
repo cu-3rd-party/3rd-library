2a0180497ce5f68eda21fc795e2aa6a836d6d2b8
Igor Mamaev <igor.mamaev1@gmail.com>
2026-01-22 01:36:30 +0300

    delete_attachment

--- backend/src/http/articles/attachments.rs
+++ backend/src/http/articles/attachments.rs
@@ -1,8 +1,8 @@
 use crate::http::ApiContext;
 use crate::http::extractor::{AuthUser, MaybeAuthUser};
-use crate::http::profiles::Profile;
 use crate::http::types::Timestamptz;
 use crate::http::{Error, Result};
+use anyhow::Context;
 use axum::extract::{Path, State};
 use axum::routing::{delete, get};
 use axum::{Json, Router};
@@ -12,11 +12,12 @@ use time::OffsetDateTime;
 use uuid::Uuid;
 
 pub fn router() -> Router<ApiContext> {
-    Router::new().route("/api/articles/{slug}/attachments", get(get_attachments))
-    // .route(
-    //     "/api/articles/{slug}/attachments/{attachment_id}",
-    //     delete(todo!()),
-    // )
+    Router::new()
+        .route("/api/articles/{slug}/attachments", get(get_attachments))
+        .route(
+            "/api/articles/{slug}/attachments/{attachment_id}",
+            delete(delete_attachment),
+        )
 }
 
 struct AttachmentFromQuery {
@@ -81,4 +82,53 @@ async fn get_attachments(
     .await?;
 
     Ok(Json(MultipleAttachments { attachments }))
+}
+
+async fn delete_attachment(
+    auth_user: AuthUser,
+    State(ctx): State<ApiContext>,
+    Path((slug, attachment_id)): Path<(String, Uuid)>,
+) -> Result<()> {
+    let result = sqlx::query!(
+        r#"
+            with deleted_attachment as (
+                delete from attachment
+                using article
+                where
+                    attachment.attachment_id = $1
+                    and attachment.article_id = article.article_id
+                    and article.slug = $2
+                    and article.user_id = $3
+                returning attachment.file_path
+            )
+            select
+                exists(
+                    select 1 from attachment
+                    inner join article using (article_id)
+                    where attachment_id = $1 and slug = $2
+                ) "existed!",
+                exists(select 1 from deleted_attachment) "deleted!",
+                (select file_path from deleted_attachment) "file_path?"
+        "#,
+        attachment_id,
+        slug,
+        auth_user.user_id,
+    )
+    .fetch_one(&ctx.db)
+    .await?;
+
+    if result.deleted {
+        if let Some(file_path) = result.file_path {
+            let path_for_error = file_path.clone();
+            tokio::task::spawn_blocking(move || std::fs::remove_file(&file_path))
+                .await
+                .context("failed to join attachment cleanup task")?
+                .with_context(|| format!("failed to delete attachment file: {}", path_for_error))?;
+        }
+        Ok(())
+    } else if result.existed {
+        Err(Error::Forbidden)
+    } else {
+        Err(Error::NotFound)
+    }
 }
