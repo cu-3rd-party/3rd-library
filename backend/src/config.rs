9e6b355c7640a22a7b418d4d7fe64f26fb86e0c5
Igor Mamaev <igor.mamaev1@gmail.com>
2026-02-02 22:18:15 +0300

    better handling of env configs

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -1,9 +1,48 @@
 use envconfig::Envconfig;
 
-#[derive(envconfig::Envconfig)]
 pub struct Config {
-    #[envconfig(from = "DATABASE_URL")]
-    pub database_url: String,
+    pub db: DbConfig,
+    pub jwt: JwtConfig,
+}
+
+impl Config {
+    pub fn init_from_env() -> Result<Self, envconfig::Error> {
+        Ok(Self {
+            db: DbConfig::init_from_env()?,
+            jwt: JwtConfig::init_from_env()?,
+        })
+    }
+}
+
+#[derive(envconfig::Envconfig)]
+pub struct JwtConfig {
     #[envconfig(from = "HMAC_KEY")]
     pub hmac_key: String,
+}
+
+#[derive(envconfig::Envconfig)]
+pub struct DbConfig {
+    #[envconfig(from = "POSTGRES_HOST")]
+    pub db_host: String,
+
+    #[envconfig(from = "POSTGRES_PORT")]
+    pub db_port: String,
+
+    #[envconfig(from = "POSTGRES_DB")]
+    pub db_name: String,
+
+    #[envconfig(from = "POSTGRES_USER")]
+    pub db_user: String,
+
+    #[envconfig(from = "POSTGRES_PASSWORD")]
+    pub db_password: String,
+}
+
+impl DbConfig {
+    pub fn database_url(&self) -> String {
+        return format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.db_user, self.db_password, self.db_host, self.db_port, self.db_name
+        );
+    }
 }
